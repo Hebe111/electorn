da287346e6fb6373cb156484941280c823648d9c
chen <17621412757@163.com>
2019-03-28 18:23:35 +0800

    更新

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,46 +10,65 @@
  *
  * @flow
  */
-import { app, BrowserWindow, Menu, Tray } from 'electron';
-import log from 'electron-log';
-import MenuBuilder from './menu';
+import { app, BrowserWindow, Menu, Tray } from "electron";
+const { dialog, autoUpdater } = require("electron");
+import log from "electron-log";
+import MenuBuilder from "./menu";
 
 export default class AppUpdater {
   constructor() {
-    log.transports.file.level = 'info';
+    log.transports.file.level = "info";
     autoUpdater.logger = log;
     autoUpdater.checkForUpdatesAndNotify();
+
+    const server = "https://hazel.17621412757.now.sh";
+    const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+    autoUpdater.setFeedURL(feed);
+    setInterval(() => {
+      autoUpdater.checkForUpdates();
+    }, 60000);
   }
 }
 
-// 更新
-const { app, autoUpdater, dialog } = require('electron');
-const server = 'https://your-deployment-url.com';
-const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
-autoUpdater.setFeedURL(feed);
-setInterval(() => {
-  autoUpdater.checkForUpdates();
-}, 60000);
+autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates."
+  };
+
+  dialog.showMessageBox(dialogOpts, response => {
+    if (response === 0) autoUpdater.quitAndInstall();
+  });
+});
+
+autoUpdater.on("error", message => {
+  console.error("There was a problem updating the application");
+  console.error(message);
+});
 
 let mainWindow = null;
 let appIcon = null;
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
+if (process.env.NODE_ENV === "production") {
+  const sourceMapSupport = require("source-map-support");
   sourceMapSupport.install();
 }
 
 if (
-  process.env.NODE_ENV === 'development' ||
-  process.env.DEBUG_PROD === 'true'
+  process.env.NODE_ENV === "development" ||
+  process.env.DEBUG_PROD === "true"
 ) {
-  require('electron-debug')();
+  require("electron-debug")();
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
+  const installer = require("electron-devtools-installer");
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+  const extensions = ["REACT_DEVELOPER_TOOLS", "REDUX_DEVTOOLS"];
 
   return Promise.all(
     extensions.map(name => installer.default(installer[name], forceDownload))
@@ -60,18 +79,18 @@ const installExtensions = async () => {
  * Add event listeners...
  */
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.DEBUG_PROD === 'true'
+    process.env.NODE_ENV === "development" ||
+    process.env.DEBUG_PROD === "true"
   ) {
     await installExtensions();
   }
@@ -92,10 +111,10 @@ app.on('ready', async () => {
     height: 670,
     minWidth: 1200,
     minHeight: 670,
-    backgroundColor: '#80FFFFFF',
-    title: '鲸交所'
+    backgroundColor: "#80FFFFFF",
+    title: "鲸交所"
   });
-  mainWindow.on('page-title-updated', function(event) {
+  mainWindow.on("page-title-updated", function(event) {
     event.preventDefault();
   });
 
@@ -103,7 +122,7 @@ app.on('ready', async () => {
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
+  mainWindow.webContents.on("did-finish-load", () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
@@ -115,7 +134,7 @@ app.on('ready', async () => {
     }
   });
 
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     console.log(111);
     mainWindow = null;
   });
