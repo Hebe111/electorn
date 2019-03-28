dd6409a668fe643ab764b17d826864164976d459
chen <17621412757@163.com>
2019-03-28 21:13:07 +0800

    update

--- app/main.dev.js
+++ app/main.dev.js
@@ -11,7 +11,8 @@
  * @flow
  */
 import { app, BrowserWindow, Menu, Tray } from "electron";
-const { dialog, autoUpdater } = require("electron");
+import { autoUpdater } from "electron-updater";
+// const { dialog, autoUpdater } = require("electron");
 import log from "electron-log";
 import MenuBuilder from "./menu";
 
@@ -21,34 +22,39 @@ export default class AppUpdater {
     autoUpdater.logger = log;
     autoUpdater.checkForUpdatesAndNotify();
 
-    const server = "https://hazel.17621412757.now.sh";
-    const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
-    autoUpdater.setFeedURL(feed);
-    setInterval(() => {
-      autoUpdater.checkForUpdates();
-    }, 60000);
+    // const server = "https://hazel.17621412757.now.sh";
+    // const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+    // console.log(11111111, feed);
+    // autoUpdater.setFeedURL(feed);
+    // setInterval(() => {
+    //   autoUpdater.checkForUpdates();
+    // }, 60000);
   }
 }
 
-autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: "info",
-    buttons: ["Restart", "Later"],
-    title: "Application Update",
-    message: process.platform === "win32" ? releaseNotes : releaseName,
-    detail:
-      "A new version has been downloaded. Restart the application to apply the updates."
-  };
-
-  dialog.showMessageBox(dialogOpts, response => {
-    if (response === 0) autoUpdater.quitAndInstall();
-  });
-});
-
-autoUpdater.on("error", message => {
-  console.error("There was a problem updating the application");
-  console.error(message);
-});
+// const server = "https://hazel.17621412757.now.sh";
+// const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+// console.log(11111111, feed);
+//
+// autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Restart", "Later"],
+//     title: "Application Update",
+//     message: process.platform === "win32" ? releaseNotes : releaseName,
+//     detail:
+//       "A new version has been downloaded. Restart the application to apply the updates."
+//   };
+//
+//   dialog.showMessageBox(dialogOpts, response => {
+//     if (response === 0) autoUpdater.quitAndInstall();
+//   });
+// });
+//
+// autoUpdater.on("error", message => {
+//   console.error("There was a problem updating the application");
+//   console.error(message);
+// });
 
 let mainWindow = null;
 let appIcon = null;
