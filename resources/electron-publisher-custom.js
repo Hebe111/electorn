fbd0e3d5a2637a98bc7458db4860e495b773cb07
chen <17621412757@163.com>
2019-03-30 13:58:47 +0800

    0.25.0

--- resources/electron-publisher-custom.js
+++ resources/electron-publisher-custom.js
@@ -1,34 +1,37 @@
-const {
-  getCiTag,
-  HttpPublisher,
-  PublishContext,
-  PublishOptions
-} = require("electron-publish/out/publisher");
-const { httpExecutor } = require("builder-util/out/nodeHttpExecutor");
-const mime = require("mime");
-const { configureRequestOptions } = require("builder-util-runtime");
+// const {
+//   getCiTag,
+//   HttpPublisher,
+//   PublishContext,
+//   PublishOptions
+// } = require("electron-publish/out/publisher");
+// const { httpExecutor } = require("builder-util/out/nodeHttpExecutor");
+// const mime = require("mime");
+// const { configureRequestOptions } = require("builder-util-runtime");
+//
+// class Publisher extends HttpPublisher {
+//   async doUpload(fileName, arch, dataLength, requestProcessor, file) {
+//     return await httpExecutor.doApiRequest(
+//       configureRequestOptions({
+//         hostname: "172.20.38.170", // TODO: from configuration
+//         protocol: "http:", // TODO: from configuration
+//         port: 8888, // TODO: from configuration
+//         path: "/${name}/${os}/${arch}/${filename}", // TODO: from configuration mixed with file meta about name/os/channel etc
+//         method: "POST",
+//         headers: {
+//           "X-File-Name": fileName,
+//           "Content-Type": mime.getType(fileName) || "application/octet-stream",
+//           "Content-Length": dataLength
+//         }
+//       }),
+//       this.context.cancellationToken,
+//       requestProcessor
+//     );
+//   }
+// }
+//
+// module.exports = {
+//   default: Publisher
+// };
 
-class Publisher extends HttpPublisher {
-  async doUpload(fileName, arch, dataLength, requestProcessor, file) {
-    return await httpExecutor.doApiRequest(
-      configureRequestOptions({
-        hostname: "my.host.name", // TODO: from configuration
-        protocol: "http:", // TODO: from configuration
-        port: 8888, // TODO: from configuration
-        path: "/some/path", // TODO: from configuration mixed with file meta about name/os/channel etc
-        method: "POST",
-        headers: {
-          "X-File-Name": fileName,
-          "Content-Type": mime.getType(fileName) || "application/octet-stream",
-          "Content-Length": dataLength
-        }
-      }),
-      this.context.cancellationToken,
-      requestProcessor
-    );
-  }
-}
-
-module.exports = {
-  default: Publisher
-};
+const Publisher = require("electron-publisher-alioss");
+module.exports = Publisher;
